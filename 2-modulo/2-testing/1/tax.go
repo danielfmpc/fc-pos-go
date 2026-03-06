6146914abd58259c07c872e25364e5fa0ecbee0a
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2026-03-05 21:24:06 -0300

    feat: implement tax calculation functions with corresponding unit and benchmark tests.

--- 2-modulo/2-testing/1/tax.go
+++ 2-modulo/2-testing/1/tax.go
@@ -1,6 +1,16 @@
 package tax
 
+import "time"
+
 func CalculateTax(amount float64) float64 {
+	if amount >= 1000 {
+		return 10.0
+	}
+	return 5.0
+}
+
+func CalculateTax2(amount float64) float64 {
+	time.Sleep(1 * time.Second)
 	if amount >= 1000 {
 		return 10.0
 	}
