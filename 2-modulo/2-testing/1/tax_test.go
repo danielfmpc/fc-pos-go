253d3cf6aaf6665ba4d86e4df64df77b22f7da5b
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2026-03-05 21:42:37 -0300

    feat: add fuzzing

--- 2-modulo/2-testing/1/tax_test.go
+++ 2-modulo/2-testing/1/tax_test.go
@@ -44,3 +44,20 @@ func BenchmarkCalculateTax2(b *testing.B) {
 		CalculateTax2(500.0)
 	}
 }
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1.0, -2, -2.5, -10.0, 0.0, 1.0, 100.0, 1000.0, 20000.0}
+	for _, s := range seed {
+		f.Add(s)
+	}
+
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f, but expected 0", result)
+		}
+		if amount > 20000 && result != 20 {
+			t.Errorf("Received %f, but expected 20", result)
+		}
+	})
+}
